fcf4d771dd3cb71ae04ab035afdf8d4d3ee16985
hayakotob <haya.kotob@mail.utoronto.ca>
2016-03-13 04:09:00 -0400

    new changes

--- functions.php
+++ functions.php
@@ -1,11 +1,7 @@
 <?php
-
 function load_google_fonts() {
 		wp_register_style('KaushanScripts','https://fonts.googleapis.com/css?family=Kaushan+Script');
             wp_enqueue_style( 'KaushanScripts'); 
-
-    
 }
 add_action('wp_print_styles', 'load_google_fonts');
 ?>
-<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
