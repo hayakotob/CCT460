2f951c2d5b6efd8dc11a99b000f2d1b8ec0e0dca
hayakotob <haya.kotob@mail.utoronto.ca>
2016-03-21 00:06:55 -0400

    Added files via upload

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: Assignment 3 Plugin
+ * Plugin URI: phoenix.sheridanc.on.ca/
+ * Description: The coolest plugin!
+ * Author: Group F- Chrishanthi Kumarasamy, Haya Kotob, Waiz Akhtar
+ * Author URI: phoenix.sheridanc.on.ca/~ccit3466
+ * Version: 1.0 */
