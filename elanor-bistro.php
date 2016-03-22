3a4a2e1ac59f24583fff4ccded45c4358ae724b6
hayakotob <haya.kotob@mail.utoronto.ca>
2016-03-22 04:04:38 -0400

    a few more changes

--- elanor-bistro.php
+++ elanor-bistro.php
@@ -6,37 +6,39 @@ Description: Pretty Fabulous Plugin
 Author: Group Four
 Author URI: http://phoenix.sheridanc.on.ca
 Version: 0.1*/
-// Creating the widget 
+
 include 'functions.php';
+// Creating the widget: Our aim is to create a widget that displays custom post types
 class ElanorWidget extends WP_Widget {
 
-function __construct() {
-parent::__construct(
-// Base ID of your widget
-'ElanorWidget', 
+		function __construct() 
+	{
+		parent::__construct(
+		// Base ID of your widget
+		'ElanorWidget', 
 
-// Widget name will appear in UI
-__('Elanor Widget', 'Elanor_Widget'), 
+		// Widget name will appear in UI
+		__('Elanor Widget', 'Elanor_Widget'), 
 
-// Widget description
-array( 'description' => __( 'This Widget will display posts from our custom post type', 'Elanor_Widget' ), ) 
-);
-}
+		// Widget description
+		array( 'description' => __( 'This Widget will display posts from our custom post type', 'Elanor_Widget' ), ) 
+		);
 
-// Creating widget front-end
+	}
+
 // This is where the action happens
 public function widget( $args, $instance ) {
+	extract ($args);
 $title = apply_filters( 'widget_title', $instance['title'] );
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-if ( ! empty( $title ) )
-echo $args['before_title'] . $title . $args['after_title'];
-
-// This is where you run the code and display the output
-echo __( 'Check this out!', 'Elanor_Widget' );
-echo $args['after_widget'];
+$numberOfItems = $instance ['numberOfItems'];
+echo $before_widget;
+if ( $title ) {
+	echo $before_title . $title . $after_title;
 }
-		
+// This is where you run the code and display the output
+$this->getElanorItems($numberOfItems);
+echo $after_widget;
+}	
 // Widget Backend 
 public function form( $instance ) {
 if ( isset( $instance[ 'title' ] ) ) {
@@ -63,13 +65,34 @@ $numberOfItems= __( '', 'Elanor_Widget' );
 		</p>
 <?php 
 }
-	
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
+		// Updating widget replacing old instances with new
+		public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+		}
+		function getElanorItems($numberOfItems) { 
+		global $post;
+		add_image_size( 'Elanor_Widget_size', 85, 45 );
+		$items = new WP_Query();
+		$items->query('post_type=listings&posts_per_page=' . $numberOfItems );
+		if($Items->found_posts > 0) {
+			echo '<ul class="Elanor_Widget">';
+				while ($Items->have_posts()) {
+					$Items->the_post();
+					$image = (has_post_thumbnail($post->ID)) ? get_the_post_thumbnail($post->ID, 'Elanor_Widget_size') : '<div class="noThumb"></div>';
+					$listItem = '<li>' . $image;
+					$listItem .= '<a href="' . get_permalink() . '">';
+					$listItem .= get_the_title() . '</a>';
+					$listItem .= '<span>Added ' . get_the_date() . '</span></li>';
+					echo $listItem;
+				}
+			echo '</ul>';
+			wp_reset_postdata();
+		}else{
+			echo '<p style="padding:25px;">No Specials Today </p>';
+		}
+	}
 } // Class Elanor_Widget ends here
 
 // Register and load the widget
